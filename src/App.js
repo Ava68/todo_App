d3ba57e9b88f8c5f4d7b6b194a3d0087d6b27e05
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-06-04 09:02:23 +0530

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-
-import './App.css';
-import React, {useState} from 'react'
+import "./App.css";
+import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Do your Homework",
+    "Drink water",
+    "Complete Assignment",
+  ]);
 
-  const [todos,setTodos] =useState(["Do your Homework",'Drink water','Complete Assignment']);
-  const [input,setInput]=useState('');
+  const [input, setInput] = useState("");
+  console.log("🔫", input);
+
+  const addTodo = (e) => {
+    e.preventDefault(); // It will stop the referesh
+    console.log("👽", "I am The alien");
+    // setTodos([...previous Array,latest input])
+    setTodos([...todos, input]);
+    setInput(""); // clearing up the input after clicking submit
+  };
 
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input  value={input} onChange={e => setInput(e.target.value)}/>
-      <button>Add Todo</button>
+      <form action="">
+        <h1>Hello</h1>
+
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        </FormControl>
+        <Button
+          disabled={!input}
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+        >
+          Add Todo
+        </Button>
+      </form>
 
       <ul>
-        {todos.map(todo =>(
+        {todos.map((todo) => (
           <li>{todo}</li>
         ))}
-        
       </ul>
     </div>
   );
